a002a6fd16b36a464b761a55e82a84014ad0543d
rjwaqas <waqassaleem631@gmail.com>
2025-06-26 16:24:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-print("🚀 App started")
+import os
 from generate_script import generate_script
 from create_video import create_video_from_images
 from text_to_speech import convert_text_to_speech
-import os
+
+print("🚀 App started")
 
 if __name__ == "__main__":
     title = os.getenv("YOUTUBE_TITLE", "Default Video Title")
-    
     script = generate_script(title)
+
+    os.makedirs("output", exist_ok=True)
     convert_text_to_speech(script, "voice.mp3")
     create_video_from_images(script, "voice.mp3", "output/output.mp4")
-    print("🎥 Video created: output.mp4")
+
+    print("🎥 Video created: output/output.mp4")
