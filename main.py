382720df0e52161eea5a452ee37653f63812426d
rjwaqas <waqassaleem631@gmail.com>
2025-06-26 14:21:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from create_video import create_video_from_images
 from text_to_speech import convert_text_to_speech
 
 if __name__ == "__main__":
-    title = input("Enter a YouTube video title: ")
+    import os
+title = os.getenv("YOUTUBE_TITLE", "Default Video Title")
 
     script = generate_script(title)
     convert_text_to_speech(script, "voice.mp3")
